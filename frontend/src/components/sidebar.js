f553a6dec4a66939048de47cf2a24bbda6eae826
educastroa <eduardocastro.a@hotmail.com>
2022-07-07 20:59:18 +0000

    minor sidebar adjustments

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -5,59 +5,66 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 export default function Navbar() {
   return (
     <main>
-      <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: 280 + 'px'}}>
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-          <span className="fs-4">Sidebar</span>
-        </a>
-
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
-              Orders
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#grid"></use></svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"><use xlinkHref="#people-circle"></use></svg>
-              Customers
-            </a>
-          </li>
-        </ul>
-
-        <div className="dropdown">
-          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"></img>
-            <strong>mdo</strong>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a className="dropdown-item" href="#">New project...</a></li>
-            <li><a className="dropdown-item" href="#">Settings</a></li>
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><hr className="dropdown-divider"></hr></li>
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
+      <div className="flex-shrink-0 p-3 bg-white" style={{width: 280 + 'px'}}>
+    <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <svg className="bi me-2" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
+      <span className="fs-5 fw-semibold">Collapsible</span>
+    </a>
+    <ul className="list-unstyled ps-0">
+      <li className="mb-1">
+        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+          Home
+        </button>
+        <div className="collapse" id="home-collapse">
+          <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" className="link-dark rounded">Overview</a></li>
+            <li><a href="#" className="link-dark rounded">Updates</a></li>
+            <li><a href="#" className="link-dark rounded">Reports</a></li>
           </ul>
         </div>
-      </div>
+      </li>
+      <li className="mb-1">
+        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          Dashboard
+        </button>
+        <div className="collapse" id="dashboard-collapse">
+          <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" className="link-dark rounded">Overview</a></li>
+            <li><a href="#" className="link-dark rounded">Weekly</a></li>
+            <li><a href="#" className="link-dark rounded">Monthly</a></li>
+            <li><a href="#" className="link-dark rounded">Annually</a></li>
+          </ul>
+        </div>
+      </li>
+      <li className="mb-1">
+        <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          Orders
+        </button>
+        <div className="collapse" id="orders-collapse">
+          <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" className="link-dark rounded">New</a></li>
+            <li><a href="#" className="link-dark rounded">Processed</a></li>
+            <li><a href="#" className="link-dark rounded">Shipped</a></li>
+            <li><a href="#" className="link-dark rounded">Returned</a></li>
+          </ul>
+        </div>
+      </li>
+      <li className="border-top my-3"></li>
+      <li className="mb-1">
+        <button className="btn btn-toggle align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="true">
+          Account
+        </button>
+        <div className="collapse show" id="account-collapse">
+          <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" className="link-dark rounded">New...</a></li>
+            <li><a href="#" className="link-dark rounded">Profile</a></li>
+            <li><a href="#" className="link-dark rounded">Settings</a></li>
+            <li><a href="#" className="link-dark rounded">Sign out</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
     </main>
   );
 }
