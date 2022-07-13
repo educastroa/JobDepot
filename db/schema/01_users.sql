fa96a440f471b9fe2416e100e2a367f1b1d37604
Heather <hmcleod29@gmail.com>
2022-07-12 21:15:38 -0400

    Fix bug in db storage

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -6,6 +6,6 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50),
-  email_address VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255)
 )
